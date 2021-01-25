d6d0c2919ea636bb08f5cc9964223c9394b385b9
BIZA-ABDELWADOUD <wadoud180@gmail.com>
2021-01-25 15:31:09 +0100

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer opened -->
+	<div class="main-footer ht-40">
+		<div class="container-fluid pd-t-0-f ht-100p">
+			<span>جميع الحقوق محفوظة © {{ date('Y') }}. تم تصميمه بواسطة <a href="https://www.linkedin.com/in/abdelwadoud-biza-378b18126/">عبد الودود بيزة</a></span>
+		</div>
+	</div>
+<!-- Footer closed -->
